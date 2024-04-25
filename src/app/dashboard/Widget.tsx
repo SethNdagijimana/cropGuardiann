6a85ac01a7a4ca4a310ab1a7466752a606ad4083
Baudouine <umutesingaruye@gmail.com>
2024-04-25 11:13:07 +0200

    fix

--- src/app/dashboard/Widget.tsx
+++ src/app/dashboard/Widget.tsx
@@ -5,18 +5,25 @@ import { User } from "@prisma/client"
 import { Header } from "@/components/Sidebar"
 
 const HeaderWidget = async () => {
+    let user = null;
 
-    const session = await getServerSession(authOptions)
-    const userId = session?.user.id as string
-  
-    const data = await getUser(userId)
-    const user = data.user as User
+    try {
+        const session = await getServerSession(authOptions);
+        const userId = session?.user?.id;
+
+        if (userId) {
+            const data = await getUser(userId);
+            user = data.user;
+        }
+    } catch (error) {
+        console.error("Error fetching user:", error);
+    }
 
     return (
         <div>
-            <Header name={user.name}/>
+            {user && <Header name={user.name} />}
         </div>
-    )
-}
+    );
+};
 
-export default HeaderWidget
+export default HeaderWidget;
